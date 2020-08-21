48973120ab525e5bcc3e9d1a7fc1f8590f9fcb4d
Esraa Jbara <jbesraa@gmail.com>
2020-08-21 18:37:19 +0300

    add channels

---
+++ channels/target/debug/deps/panama-3c3cf8ade69dcd92.d
@@ -0,0 +1,5 @@
+/Users/esraajbara/Documents/learn_rust/panama/target/debug/deps/panama-3c3cf8ade69dcd92: src/lib.rs
+
+/Users/esraajbara/Documents/learn_rust/panama/target/debug/deps/panama-3c3cf8ade69dcd92.d: src/lib.rs
+
+src/lib.rs:
