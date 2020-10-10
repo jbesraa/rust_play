3f35d0793bd9c48867428fe47bea048468e1257f
Esraa Jbara <jbesraa@gmail.com>
2020-10-10 12:26:09 +0300

    add transactioninput/output structs

--- utxo-workshop/runtime/src/utxo.rs
+++ utxo-workshop/runtime/src/utxo.rs
@@ -13,7 +13,44 @@ use sp_runtime::traits::{BlakeTwo256, Hash, SaturatedConversion};
 use sp_std::collections::btree_map::BTreeMap;
 use sp_runtime::transaction_validity::{TransactionLongevity, ValidTransaction};
 
-// TODO 1: Import missing dataypes
+/// Single transaction to be dispatched
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+#[derive(PartialEq, Eq, PartialOrd, Ord, Default, Clone, Encode, Decode, Hash, Debug)]
+pub struct Transaction {
+    /// UTXOs to be used as inputs for current transaction
+    pub inputs: Vec<TransactionInput>,
+
+    /// UTXOs to be created as a result of current transaction dispatch
+    pub outputs: Vec<TransactionOutput>,
+}
+
+/// Single transaction input that refers to one UTXO
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+#[derive(PartialEq, Eq, PartialOrd, Ord, Default, Clone, Encode, Decode, Hash, Debug)]
+pub struct TransactionInput {
+    /// Reference to an UTXO to be spent
+    pub outpoint: H256,
+
+    /// Proof that transaction owner is authorized to spend referred UTXO & 
+    /// that the entire transaction is untampered
+    pub sigscript: H512,
+}
+
+
+/// Single transaction output to create upon transaction dispatch
+#[cfg_attr(feature = "std", derive(Serialize, Deserialize))]
+#[derive(PartialEq, Eq, PartialOrd, Ord, Default, Clone, Encode, Decode, Hash, Debug)]
+pub struct TransactionOutput {
+    /// Value associated with this output
+    pub value; Value,
+
+    /// public key associated with this output.
+    /// In order to spend this output woner must provide
+    /// a proof by hashing the whole Transaction and
+    /// signing it with a corresponding private key.
+    pub pubkey: H256,
+}
+
 
 pub trait Trait: system::Trait {
     type Event: From<Event> + Into<<Self as system::Trait>::Event>;
